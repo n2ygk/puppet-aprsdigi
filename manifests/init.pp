8396cd05ce0874980575c9f2fd19ece85871cc5d
Alan Crosswell <alan@crosswell.us>
2016-10-28 19:33:41 -0400

    nonworking but want to stash a copy

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,9 +25,33 @@
 # Copyright 2016 Alan Crosswell
 #
 class aprsdigi (
-  $callsign                     = $aprsdigi::params::callsign,
+  $callsign     = $aprsdigi::params::callsign,
+  $intf         = $aprsdigi::params:intf,
+  $aliases	= $aprsdigi::params:aliases,
+  $lat		= $aprsdigi::params:lat,
+  $lon		= $aprsdigi::params:lon,
+  $phg		= $aprsdigi::params:phg,
+  $txt		= $aprsdigi::params:txt,
+  $dst		= $aprsdigi::params:dst,
+  $logfile	= $aprsdigi::params:logfile,
+  $kill_dupes	= $aprsdigi::params:kill_dupes,
+  $kill_loops	= $aprsdigi::params:kill_loops,
+  $subst_mycall	= $aprsdigi::params:subst_mycall,
+  $x1j4_xlate	= $aprsdigi::params:x1j4_xlate,
 ) inherits aprsdigi::params {
   validate_string($callsign)
+  validate_string($intf)  
+  validate_string($aliases)
+  validate_string($lat)
+  validate_string($lon)
+  validate_string($phg)
+  validate_string($txt)
+  validate_string($dst)
+  validate_absolute_path($logfile)
+  validate_bool($kill_dupes)
+  validate_bool($kill_loops)
+  validate_bool($subst_mycall)
+  validate_bool($x1j4_xlate)
 
   anchor { 'aprsdigi::begin': } ->
   class { '::aprsdigi::install': } ->
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,15 @@
 class aprsdigi::params {
-  $intf   = 'sm0'
-  $pttdev = '/dev/ttyS0'
-  $callsign   = undef
-  $audio_type  = 'alsa'
-  $alsa_dev = 'plughw:0,0'
-  $alsa_speaker_playback_volume = 15
-  $alsa_pcm_capture_volume = 4096 # 0 dB
+  $callsign = undef,
+  $intf = "sm0",
+  $aliases = "RELAY,TRACE,WIDE1-1,TRACE1-1",
+  $lat = undef, #example: "4118.  N" (blanks for location ambiguity)
+  $lon = undef, #example: "07353.  W"
+  $phg = undef, # power height gain: "PHG7300"
+  $txt = undef, # "fill in digipeater www.weca.org"
+  $dst = "APRS via WIDE2-2", # default for a fill-in digipeater.
+  $logfile = "/var/log/aprsdigi.log",
+  $kill_dupes = true,
+  $kill_loops = true,
+  $subst_mycall = true,
+  $x1j4_xlate = true,
 }
